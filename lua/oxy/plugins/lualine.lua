ec54c242c3d6a721f4de3b0a3f544b2b9b529659
g3ntl3m3n <oxymje@gmail.com>
2024-01-25 00:04:23 +0000

    colorizer, lualine colorscheme, autopairs, telescope

--- lua/oxy/plugins/lualine.lua
+++ lua/oxy/plugins/lualine.lua
@@ -1,11 +1,71 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = {"nvim-tree/nvim-web-devicons"},
-  config = function ()
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
     local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
+    local colors = {
+      blue = "#81a2be",
+      green = "#b5bd68",
+      violet = "#b294bb",
+      yellow = "#f0c674",
+      red = "#cc6666",
+      fg = "#c5c8c6",
+      bg = "#232c31",
+      inactive_bg = "#1d1f21",
+    }
+
+    local my_lualine_theme = {
+      normal = {
+        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      insert = {
+        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      visual = {
+        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      command = {
+        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      replace = {
+        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      inactive = {
+        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+      },
+    }
+
+    -- configure lualine with modified theme
     lualine.setup({
-      options = {theme = "auto"} 
+      options = {
+        theme = my_lualine_theme,
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
     })
   end,
 }
